5aff8a4d691003a8cc4bc7e2695829d75263b866
Test-Github-explore <158149339+Test-Github-explore@users.noreply.github.com>
2024-02-15 15:36:10 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,30 +1,3 @@
-/*#include<stdio.h>
-
-int main(void){
-   int id_sequence[3];
-
-
-   id_sequence[0] = 123;
-   id_sequence[1] = 234;
-   id_sequence[2] = 345;
-   id_sequence[3] = 456;
-   return 0;
-}
-
-void str04(){
-
-   size_t len;
-   char cstr[] = "char string";
-   signed char scstr[] = "signed char string";
-   unsigned char ucstr[] = "unsigned char string";
-
-   len = strlen(cstr);
-   len = strlen(scstr);  /* char が符号無しならば警告する */
-   len = strlen(ucstr);  /* char が符号付きならば警告する */
-
-   return ;
-}*/
-
 #include <stdlib.h>
  
 struct node {
