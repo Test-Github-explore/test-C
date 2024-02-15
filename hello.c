a074078b1b82cf9d8a0f624156d9acf2a7de4f25
Test-Github-explore <158149339+Test-Github-explore@users.noreply.github.com>
2024-02-15 15:34:20 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+/*#include<stdio.h>
 
 int main(void){
    int id_sequence[3];
 
-/* Populate the id array. */
+
    id_sequence[0] = 123;
    id_sequence[1] = 234;
    id_sequence[2] = 345;
@@ -12,7 +12,7 @@ int main(void){
 }
 
 void str04(){
-/* STR04-C:基本文字集合にある文字を表すには単なる char を使用する START*/
+
    size_t len;
    char cstr[] = "char string";
    signed char scstr[] = "signed char string";
@@ -21,8 +21,21 @@ void str04(){
    len = strlen(cstr);
    len = strlen(scstr);  /* char が符号無しならば警告する */
    len = strlen(ucstr);  /* char が符号付きならば警告する */
-/* STR04-C END*/
+
    return ;
+}*/
+
+#include <stdlib.h>
+ 
+struct node {
+  int value;
+  struct node *next;
+};
+ 
+void free_list(struct node *head) {
+  for (struct node *p = head; p != NULL; p = p->next) {
+    free(p);
+  }
 }
    
 
