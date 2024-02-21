17421316366d367989af17a7f9312170c6531851
Test-Github-explore <158149339+Test-Github-explore@users.noreply.github.com>
2024-02-21 11:01:26 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,10 +1,22 @@
-int main(char * s, unsigned size) {
-	char* buf = (char*)malloc(size);
+int main(int argc, char** argv) {
+  char *userAndFile = argv[2];
+  
+  {
+    char fileBuffer[FILENAME_MAX] = "/home/";
+    char *fileName = fileBuffer;
+    size_t len = strlen(fileName);
+    strncat(fileName+len, userAndFile, FILENAME_MAX-len-1);
+    // BAD: a string from the user is used in a filename
+    fopen(fileName, "wb+");
+  }
 
-	strncpy(buf, s, size + 1); // wrong: copy may exceed size of buf
-
-	for (int i = 0; i <= size; i++) { // wrong: upper limit that is higher than size of buf
-		cout << buf[i];
-	}
-  return ;
+  {
+    char fileBuffer[FILENAME_MAX] = "/home/";
+    char *fileName = fileBuffer;
+    size_t len = strlen(fileName);
+    // GOOD: use a fixed file
+    char* fixed = "jim/file.txt";
+    strncat(fileName+len, fixed, FILENAME_MAX-len-1);
+    fopen(fileName, "wb+");
+  }
 }
